a65ff81bce6fe5a478ba35d2932638546f2965b5
Begzod2004 <mirzaqulovbekmurod@gmail.com>
2023-06-25 17:05:42 +0500

    adedd

--- config/settings.py
+++ config/settings.py
@@ -59,9 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   
-    # Custom middleware
-    'core.middleware.RevisionMiddleware',
 ]
 
 
