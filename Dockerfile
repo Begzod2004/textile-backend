1346d243aa2f53420b9b7a51f434e0535b1e061d
Begzod2004 <mirzaqulovbekmurod@gmail.com>
2023-06-29 11:59:46 +0500

    staticfiles added-3

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.8
-ENV DockerHOME=/home/app/webapp
+ENV DockerHOME=/
 
 RUN mkdir -p $DockerHOME
 
@@ -13,4 +13,4 @@ RUN pip install --upgrade pip
 COPY . $DockerHOME
 RUN pip install -r requirements.txt
 EXPOSE 8000
-CMD python manage.py runserver
+CMD python manage.py runserver 0.0.0.0:8000
--- manage.py
+++ manage.py
@@ -20,3 +20,25 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    import os
+    import sys
+
+    # local_settings.py faylini import qiling
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.local_settings')
+
+    # serverning o'zgaruvchilarni sozlang
+    os.environ.setdefault('DJANGO_ALLOW_ASYNC_UNSAFE', 'true')
+    os.environ.setdefault('RUN_MAIN', 'true')
+
+    # serverni ishga tushirish
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    execute_from_command_line(sys.argv)
